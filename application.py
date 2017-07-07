360c559a04af70eb2c2dee238e3fa75d5ed5d3d4
Lucy C Guo <sr.lguo@students.srvusd.net>
2017-07-07 15:03:14 +0000

    set up Postgres

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import psycopg2
 import os
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, url_for
@@ -30,7 +31,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL(os.environ.get(“DATABASE_URL”) or “sqlite:///finance.db”)
 
 @app.route("/")
 @login_required
